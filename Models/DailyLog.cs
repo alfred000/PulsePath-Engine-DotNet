91f44138a1997f5c56a89292b9250330ce7aea38
Alfred Nzigiye <alfred.nzigiye@gmail.com>
2026-05-06 13:12:32 +0200

    Initial project structure

---
+++ Models/DailyLog.cs
@@ -0,0 +1,12 @@
+namespace PulsePath.Models
+{
+    public class DailyLog
+    {
+        public DateTime Date { get; set; }
+        public double Weight { get; set; }
+        public int CaloriesIn { get; set; }
+        public int Steps { get; set; }
+        public double SleepHours { get; set; }
+        // We will add Protein and Fasting later (V1.1)
+    }
+}
